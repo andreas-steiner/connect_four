1cf38fe28a92c7f221f889dd1e2cb677ccde2f7e
claudiomartinuzzi <claudio.martinuzzi@edu.campus02.at>
2021-02-11 12:19:45 +0100

    Update test_main.py
    Added def setUP(self):

--- test_main.py
+++ test_main.py
@@ -4,11 +4,52 @@ from main import Board, PlayBoard
 
 class test_game(unittest.TestCase):
 
-    def test_make_board(self):
+
+
+
+    def setUp(self):
+
+        self.blank_game_state_1 = [
+            [None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+        ]
+
+        self.test_game_state_2 = [
+            [None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+            [None, None, "2", "1", None, None, None],
+            [None, None, "1", "2", None, None, None],
+            ["1", "2", "2", "1", "2", "1", "2"],
+            ["1", "1", "2", "2", "1", "2", "1"],
+        ]
+
+        self.test_game_stat_3 = [
+            [None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None],
+            [None, None, "2", "1", None, None, None],
+            [None, None, "2", "1", None, None, None],
+            ["1", "2", "2", "1", "2", "1", "2"],
+            ["1", "1", "2", "2", "1", "2", "1"],
+        ]
+
+
+
+    def test_win_cond(self):
         pass
 
     def test_place_stone(self):
         pass
+
+    def test_make_board(self):
+        #board = PlayBoard.make_board(self)
+        #self.assertEqual(board, np.zeros(7, 6))
+        pass
+
+
 
     def test_loc_valid(self):
         pass
@@ -16,10 +57,10 @@ class test_game(unittest.TestCase):
     def test_next_row(self):
         pass
 
-    def test_win_cond(self):
-        pass
+
 
     
 
 if __name__ == '__main__':
     unittest.main()
+
