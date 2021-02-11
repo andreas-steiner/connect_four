9f47262e72584547297d7236c8b190898da9d966
claudiomartinuzzi <claudio.martinuzzi@edu.campus02.at>
2021-02-11 09:33:09 +0100

    Create test_main.py
    UnitTest Skelett angelegt

---
+++ test_main.py
@@ -0,0 +1,25 @@
+import unittest
+from main import Board, PlayBoard
+
+
+class test_game(unittest.TestCase):
+
+    def test_make_board(self):
+        pass
+
+    def test_place_stone(self):
+        pass
+
+    def test_loc_valid(self):
+        pass
+
+    def test_next_row(self):
+        pass
+
+    def test_win_cond(self):
+        pass
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
